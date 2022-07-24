82d2ac76261ec1a5794870cebf99e516c052999f
Adrian864 <adrian864bg@gmail.com>
2022-07-24 13:20:52 +0200

    Switch + account details

--- src/main/java/AccountDetails.java
+++ src/main/java/AccountDetails.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class AccountDetails {
     private String name;
     private String accountNumber;
     private String login;
     private long balance;
+    Scanner scanner = new Scanner(System.in);
+    public void createAccount(){
+        System.out.println("Podaj swoje Imię: ");
+        name = scanner.next();
+        System.out.println("Podaj numer konta: ");
+        accountNumber = scanner.next();
+        System.out.println("Podaj login: ");
+        login = scanner.next();
+        System.out.println("Podaj stan konta: ");
+        balance = scanner.nextLong();
+    }
 
     public void showAccount(){
         System.out.println("Your account name is: "+ name);
